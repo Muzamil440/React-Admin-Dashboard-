3c430bc356fb9e928f807f9cd94b74c3d5b3e077
Muzamil440 <158570712+Muzamil440@users.noreply.github.com>
2024-07-19 05:52:03 +0500

    Update DataTable.tsx

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -24,10 +24,10 @@ const DataTable = (props: Props) => {
   // //   }
   // // });
 
-  const handleDelete = (id: number) => {
-    //delete the item
-    // mutation.mutate(id)
-  };
+   // const handleDelete = (id: number) => {
+   //  delete the item
+   //  mutation.mutate(id)
+   // };
 
   const actionColumn: GridColDef = {
     field: 'action',
